dff7688a1d4f76b920721240f634b313796958be
Renato <72624715+renatoka@users.noreply.github.com>
2020-10-26 13:27:46 +0100

    Update redditReplyBot_1.py

--- redditReplyBot_1.py
+++ redditReplyBot_1.py
@@ -21,6 +21,8 @@ already_exist = True
 
 
 for submission in subreddit.new(limit = 5):
+    ## THIS PART IS KNOWN TO GET BUGGY SO IF YOU DON'T WANT TO CHECK
+    ## POST TITLE'S JUST COMMENT OUT FROM HERE
     for naslov in submission.title:
         while (already_exist != False):
             #Edit your own trigger TITLE word 
@@ -34,7 +36,7 @@ for submission in subreddit.new(limit = 5):
             else:
                 print("Already commented on it or post with trigger words doesn't exist.")
                 already_exist = False
-
+    ## TO HERE ^^
     for comment in submission.comments:
         if hasattr(comment, "body"):
             comment_lower = comment.body.lower()
@@ -51,64 +53,3 @@ for submission in subreddit.new(limit = 5):
                 print("I replied on thread named : {0}".format(submission.title))
                 print("---------------")
                 time.sleep(800)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            #if " omjer cijene " in comment_lower:
-            #    print("-------")
-            #    print("Name of thread : {0}".format(submission.title))
-            #    print("Comment : {0}".format(comment.body))
-            #    print("Author of the comment : {0}".format(comment.author))
-
-                #if (reply_phrase in comment.body):
-                #    print("Reply phrase found, skipping this comment.")
-
-                #else:
-                #    comment.reply(reply_phrase)
-                #    print("Word ""omjer cijene"" was found in this comment : {0}".format(comment_lower))
-                #    print("---------------")
-                #    print("I replied on thread named : {0}".format(submission.title))
-                #    print("---------------")
-                #    time.sleep(10)
-
-
-    #for naslov in submission.title:
-    #    izabrana_rijec = random.randint(0,len(rijeci))
-    #    if str(izabrana_rijec) in submission.title:
-    #        submission.reply(reply_phrase)
-    #        print("Jedna od rijeci je pronadena u naslovu ovog threada : {0}".format(submission.title))
-    #        print("Odgovorio sam na thread pod imenom {0} ".format(submission.title))
-    #        print("---------------")
-    #        time.sleep(30)
-
-    #for naslov in submission.title:
-    #    if "Gdje" in submission.title:
-    #        submission.reply("Trazis najbolji omjer cijene i kvalitete? Pa to vec i ptice na grani znaju, Xiaomi.")
-    #        time.sleep(780)
-
